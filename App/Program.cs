50e326a348173335b9f94d4b9bbf6361ec1d3f27
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-20 12:17:05 -0400

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -4,8 +4,9 @@ class Program
 {
     public static void Main()
     {
-        int[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
+        int[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
         var y = Utils.Build(x,0,x.Length);
-        System.Console.WriteLine(Utils.LCA(4,3,y).Value);
+
+        System.Console.WriteLine(y.SubtreeWhere(p=>p%2==0));
     }
 }
--- Library/Arboles/Ejercicios.cs
+++ Library/Arboles/Ejercicios.cs
@@ -2,5 +2,24 @@ namespace Trees;
 
 public static class Ejercicios
 {
-    
+    public static IEnumerable<Arbol<T>> SubtreeWhere<T>(this Arbol<T> tree, Predicate<T> pred) => tree.DFSTree().Where(x => pred(x.Value));
+    public static IEnumerable<Arbol<T>> MaximalSubtreesWhere<T>(Arbol<T> tree, Predicate<T> predicate)
+    {
+        List<Arbol<T>> anadidos = new();
+
+        foreach (var item in tree.DFSTree())
+        {
+            if (anadidos.Exists(x => x.Children.Contains(item)))
+            {
+                anadidos.Add(item);
+                continue;
+            }
+
+            if (item.DFSTree().All(x => predicate(x.Value)))
+            {
+                yield return item;
+                anadidos.Add(item);
+            }
+        }
+    }
 }
