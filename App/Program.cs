ab06b8f2544842ce30c339418fc3082d3bea8928
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-20 11:22:39 -0400

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     public static void Main()
     {
-        int[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
         var y = Utils.Build(x,0,x.Length);
+        System.Console.WriteLine(string.Join(" ", y.DFS()));
+        System.Console.WriteLine(string.Join(" ", y.BFS()));
+        System.Console.WriteLine(y.Height);
+        System.Console.WriteLine(y.Diameter);
     }
 }
--- Library/Arboles/Trees.cs
+++ Library/Arboles/Trees.cs
@@ -10,6 +10,67 @@ public class Arbol<T>
 
     public T Value { get; set; }
     public IEnumerable<Arbol<T>> Children { get; set; }
+    public int Height
+    {
+        get
+        {
+            int o = 0;
+            foreach (var item in Children)
+                if (item != null)
+                    o = Math.Max(item.Height, o);
+            return o + 1;
+        }
+    }
+
+    public int Diameter
+    {
+        get
+        {
+            int d = 0, h1 = 0, h2 = 0;
+            foreach (var item in Children)
+            {
+                if (item != null)
+                {
+                    d = Math.Max(d, item.Diameter);
+
+                    if (item.Height > h1)
+                    {
+                        h2 = h1;
+                        h1 = item.Height;
+                    }
+                    else if (item.Height > h2)
+                    {
+                        h2 = item.Height;
+                    }
+                }
+            }
+            return Math.Max(d, h1 + h2 + 1);
+        }
+    }
+    public IEnumerable<T> DFS()
+    {
+        yield return Value;
+        foreach (var item in Children)
+        {
+            if (item != null)
+                foreach (var item2 in item.DFS())
+                    yield return item2;
+        }
+    }
+    public IEnumerable<T> BFS()
+    {
+        Queue<Arbol<T>> cola = new();
+        cola.Enqueue(this);
+        while (cola.Any())
+        {
+            yield return cola.Peek().Value;
+            foreach (var item in cola.Peek().Children)
+                if (item != null)
+                    cola.Enqueue(item);
+            cola.Dequeue();
+        }
+    }
+
 }
 
 public static class Utils
@@ -17,9 +78,9 @@ public static class Utils
     public static Arbol<T> Build<T>(T[] array, int start, int end)
     {
         if (start == end) return null!;
-        int mid = (start+end)/2;
-        var x = Build(array,start,mid);
-        var y = Build(array,mid+1,end);
-        return new Arbol<T>(array[mid],x,y);
+        int mid = (start + end) / 2;
+        var x = Build(array, start, mid);
+        var y = Build(array, mid + 1, end);
+        return new Arbol<T>(array[mid], x, y);
     }
 }
