838092b5a0c9d3ef447a973d5829c98af519136a
vekt0R-HUB <d0t42f4n@gmail.com>
2022-07-20 11:48:13 -0400

    Update

--- App/Program.cs
+++ App/Program.cs
@@ -6,9 +6,6 @@ class Program
     {
         int[] x = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17};
         var y = Utils.Build(x,0,x.Length);
-        System.Console.WriteLine(string.Join(" ", y.DFS()));
-        System.Console.WriteLine(string.Join(" ", y.BFS()));
-        System.Console.WriteLine(y.Height);
-        System.Console.WriteLine(y.Diameter);
+        System.Console.WriteLine(Utils.LCA(4,3,y).Value);
     }
 }
--- Library/Arboles/Trees.cs
+++ Library/Arboles/Trees.cs
@@ -51,11 +51,9 @@ public class Arbol<T>
     {
         yield return Value;
         foreach (var item in Children)
-        {
             if (item != null)
                 foreach (var item2 in item.DFS())
                     yield return item2;
-        }
     }
     public IEnumerable<T> BFS()
     {
@@ -70,7 +68,16 @@ public class Arbol<T>
             cola.Dequeue();
         }
     }
-
+    public IEnumerable<Arbol<T>> DFSTree()
+    {
+        yield return this;
+        foreach (var item in Children)
+            if (item != null)
+                foreach (var item2 in item.DFSTree())
+                    yield return item2;
+    }
+    public bool Contains(T x) => DFS().Contains(x);
+    public bool Contains(Arbol<T> x) => DFSTree().Contains(x);
 }
 
 public static class Utils
@@ -82,5 +89,19 @@ public static class Utils
         var x = Build(array, start, mid);
         var y = Build(array, mid + 1, end);
         return new Arbol<T>(array[mid], x, y);
+    }
+    public static Arbol<T> LCA<T>(Arbol<T> x1, Arbol<T> x2, Arbol<T> raiz)
+    {
+        foreach (var item in raiz.Children)
+            if (item.Contains(x1) && item.Contains(x2))
+                return LCA(x1, x2, item);
+        return raiz;
+    }
+    public static Arbol<T> LCA<T>(T x1, T x2, Arbol<T> raiz)
+    {
+        foreach (var item in raiz.Children)
+            if (item.Contains(x1) && item.Contains(x2))
+                return LCA(x1, x2, item);
+        return raiz;
     }
 }
